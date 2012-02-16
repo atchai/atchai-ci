94fcabc999add8cca5cd3a3d04512a25da5c2cbd
Rob Hardwick <robhardwick@gmail.com>
2012-02-16 12:57:20 +0000

    Creation deploy class and ignore drush errors

---
+++ drupal.py
@@ -0,0 +1,25 @@
+from fabric.context_managers import cd
+from fabric.context_managers import settings
+from fabric.operations import run
+
+class deploy:
+    def __init__(self, path, repo, public_path='/public',
+                 db_user='test', db_pass='test', db_host='localhost', db_name='test',
+                 admin_user='Admin', admin_pass='password', site_name='Test'):
+        self.public = path + public_path
+        self.pull(path, repo)
+        self.install(db_user, db_pass, db_host, db_name, site_name, admin_user, admin_pass)
+
+    def install(self, db_user, db_pass, db_host, db_name, site_name, admin_user, admin_pass):
+        self.drush('si minimal --db-url=mysql://%s:%s@%s/%s --site-name="%s" --account-name="%s" --account-pass="%s"'
+            % (db_user, db_pass, db_host, db_name, site_name, admin_user, admin_pass,))
+
+    def pull(self, path, repo):
+        with settings(warn_only=True):
+            if run('test -d %s/.git' % path).failed:
+                run('git clone %s %s' % (repo, path,))
+        run('cd %s; git pull' % path)
+
+    def drush(self, cmd, ignore_error=True):
+        with settings(warn_only=ignore_error):
+            run('drush --root="%s" %s -y' % (self.public, cmd,))
--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,18 @@
-from fabric.context_managers import cd
-from fabric.context_managers import settings
-from fabric.operations import run
+from fabric.state import env
+import drupal
 
-def deploy(repo='', path='/var/www/test',
-           db_user='test', db_pass='test', db_host='localhost', db_name='test',
-           admin_user='Admin', admin_pass='password', site_name='Test'):
-    pull(repo, path)
-    install(path, db_user, db_pass, db_host, db_name, admin_user, admin_pass, site_name)
+def prod():
+    env['repo_path'] = '/var/www/drupal';
+    env.hosts = ['example.com']
 
-def pull(repo, path):
-    with settings(warn_only=True):
-        if run('test -d %s/.git' % path).failed:
-            run('git clone %s %s' % (repo, path,))
-    run('cd %s; git pull' % path)
+def test():
+    env['repo_path'] = '/var/www/test';
+    env.hosts = ['example.com']
 
-def install(path, db_user, db_pass, db_host, db_name, admin_user, admin_pass, site_name):
-    run('drush --root="%s/public" si minimal --db-url=mysql://%s:%s@%s/%s --site-name="%s" --account-name="%s" --account-pass="%s" -y'
-        % (path, db_user, db_pass, db_host, db_name, site_name, admin_user, admin_pass,))
+def dev():
+    env['repo_path'] = '/var/www/drupal';
+    env.hosts = ['localhost']
+
+def deploy():
+    deploy = drupal.deploy(path=env['repo_path'], repo='git://github.com/example/example.git')
+    deploy.drush('en example')
