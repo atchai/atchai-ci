bb827f178ec93633932404d34c5cd676d06821a6
Rob Hardwick <robhardwick@gmail.com>
2012-02-16 16:33:20 +0000

    Split out tests and simplify environment variable inheritance

--- drupal.py
+++ drupal.py
@@ -3,16 +3,9 @@ from fabric.context_managers import settings
 from fabric.operations import run
 
 class deploy:
-    def __init__(self, path, repo, public_path='/public',
-                 db_user='test', db_pass='test', db_host='localhost', db_name='test',
-                 admin_user='Admin', admin_pass='password', site_name='Test'):
+    def __init__(self, path, repo, public_path='/public')
         self.public = path + public_path
         self.pull(path, repo)
-        self.install(db_user, db_pass, db_host, db_name, site_name, admin_user, admin_pass)
-
-    def install(self, db_user, db_pass, db_host, db_name, site_name, admin_user, admin_pass):
-        self.drush('si minimal --db-url=mysql://%s:%s@%s/%s --site-name="%s" --account-name="%s" --account-pass="%s"'
-            % (db_user, db_pass, db_host, db_name, site_name, admin_user, admin_pass,), False)
 
     def pull(self, path, repo):
         with settings(warn_only=True):
@@ -20,6 +13,15 @@ class deploy:
                 run('git clone %s %s' % (repo, path,))
         run('cd %s; git pull' % path)
 
-    def drush(self, cmd, ignore_error=True):
+    def drush(self, cmd, ignore_error=False):
         with settings(warn_only=ignore_error):
             run('drush --root="%s" %s -y' % (self.public, cmd,))
+
+    def install(self, db_user='test', db_pass='test', db_host='localhost', db_name='test',
+                site_name='Test', admin_user='Admin', admin_pass='password'):
+        self.drush('si minimal --db-url=mysql://%s:%s@%s/%s --site-name="%s" --account-name="%s" --account-pass="%s"'
+            % (db_user, db_pass, db_host, db_name, site_name, admin_user, admin_pass,))
+
+    def test(self, url, php='/usr/bin/php'):
+        self.drush('en simpletest')
+        run('cd %s; php ./scripts/run-tests.sh --php %s --url %s --all' % (self.public, php, url,))
---
+++ fabfile-example.py
@@ -0,0 +1,28 @@
+from fabric.state import env
+import drupal
+
+def prod():
+    env['repo_url'] = 'git://github.com/example/example.git'
+    env['repo_path'] = '/var/www/drupal'
+    env['url'] = 'http://example.com/'
+    env['hosts'] = ['example.com']
+
+def test():
+    prod()
+    env['repo_path'] = '/var/www/test'
+    env['url'] = 'http://test.example.com/'
+
+def dev():
+    prod()
+    env['repo_path'] = '/var/www/drupal'
+    env['url'] = 'http://localhost/'
+    env['hosts'] = ['localhost'
+
+def deploy():
+    deploy = drupal.deploy(env['repo_path'], env['repo_url'])
+    deploy.install()
+    deploy.drush('en example')
+
+def test():
+    deploy = drupal.deploy(env['repo_path'], env['repo_url'])
+    deploy.test(env['url'])
